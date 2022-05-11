16ef87056d10c325c6c90aebb0adaca21719b0f4
Chad Stabler <stablerpsn@gmail.com>
2022-05-11 16:43:51 -0500

    functionality up, data loading on home page

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
+import { getBosses } from './fetch-utils.js';
+import { renderBoss } from './render-utils.js';
 // import functions and grab DOM elements
-
+const bossEl = document.getElementById('boss-section');
 // let state
 
+async function displayData() {
+    const bosses = await getBosses();
+
+    for (let boss of bosses) {
+        const newDiv = renderBoss(boss);
+        bossEl.append(newDiv);
+    }
+}
+displayData();
 // set event listeners 
   // get user input
   // use user input to update state 
--- render-utils.js
+++ render-utils.js
@@ -1,18 +1,18 @@
 export function renderBoss(boss) {
     const newEl = document.createElement('a');
     const bossDiv = document.createElement('div');
-    const imageEl = document.createElement('img');
+    //const imageEl = document.createElement('img');
     const textEl = document.createElement('p');
     
     newEl.classList.add('boss');
 
-    imageEl.src = boss.image;
+    //imageEl.src = boss.image;
 
     textEl.textContent = boss.name;
 
     newEl.href = `./detail/index.html?id=${boss.id}`;
 
-    bossDiv.append(imageEl, textEl);
+    bossDiv.append(/*imageEl,*/ textEl);
 
     newEl.append(bossDiv);
 
